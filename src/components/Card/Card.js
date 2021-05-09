0367eb1835f9dd882757f3d46afa3659b5d32fc2
Stephanie Norton <steph.jane.norton@gmail.com>
2021-05-09 09:25:19 -0600

    Add on click toggle between front and back images

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,13 @@
+import React, { useState } from 'react'
 import './Card.css';
 
 const Card = ({ card }) => {
+  const [ viewFront, setViewFront ] = useState(true)
+
   return (
-    <img className='card' src={card.image} alt='front of card' />
-  );
+    <img className='card' src={viewFront ? card.imageFront : card.imageBack} alt={`front of ${card.name} card`} onClick={() => setViewFront(!viewFront)}/>
+
+  )
 }
 
 export default Card;
