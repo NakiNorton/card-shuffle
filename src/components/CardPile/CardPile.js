b7bf9d771cce21e3742670382cdf57a5638ac92f
Stephanie Norton <steph.jane.norton@gmail.com>
2021-05-09 12:51:13 -0600

    Create CardPile component

---
+++ src/components/CardPile/CardPile.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './CardPile.css';
+
+const CardPile = ({ deck }) => {
+  const [ topCardIndex, setTopCardIndex ] = useState(0)
+
+  return (
+    <>
+      <div className='card-pile'>
+        {topCardIndex === deck.length && 
+          <h2 className='user-msg'>You've reached the end of the pile!</h2>
+        }
+        {topCardIndex !== deck.length &&
+          <img className='card-pile-card' src={deck[topCardIndex].imageBack} alt={'top card on the deck'} onClick={() => setTopCardIndex(topCardIndex + 1)} />
+        }
+      </div>
+
+      <div className='card-active'>
+        {!topCardIndex &&
+          <h2 className='user-msg'>Click on a card to flip it over!</h2>
+        }
+        {!!topCardIndex &&
+          <img className='card-pile-card' src={deck[topCardIndex - 1].imageFront} alt='flipped card' />
+        }
+      </div>
+    </>
+  )
+}
+
+export default CardPile;
