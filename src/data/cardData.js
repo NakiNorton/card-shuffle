5ede20e2a4f5984aaf5c9e9010556e53c9fd6dad
Stephanie Norton <steph.jane.norton@gmail.com>
2021-05-09 09:22:54 -0600

    Add back of card image to card objects

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,262 +1,314 @@
 export const cardData = [
   {
     id: 1,
-    image: 'assets/blue-01.png',
+    imageFront: 'assets/blue-01.png',
+    imageBack: 'assets/back.png',
     name: 'blue A'
   },
   {
     id: 2,
-    image: 'assets/blue-02.png',
+    imageFront: 'assets/blue-02.png',
+    imageBack: 'assets/back.png',
     name: 'blue 2'
   },
   {
     id: 3,
-    image: 'assets/blue-03.png',
+    imageFront: 'assets/blue-03.png',
+    imageBack: 'assets/back.png',
     name: 'blue 3'
   },
   {
     id: 4,
-    image: 'assets/blue-04.png',
+    imageFront: 'assets/blue-04.png',
+    imageBack: 'assets/back.png',
     name: 'blue 3'
   },
   {
     id: 5,
-    image: 'assets/blue-05.png',
+    imageFront: 'assets/blue-05.png',
+    imageBack: 'assets/back.png',
     name: 'blue 5'
   },
   {
     id: 6,
-    image: 'assets/blue-06.png',
+    imageFront: 'assets/blue-06.png',
+    imageBack: 'assets/back.png',
     name: 'blue 6'
   },
   {
     id: 7,
-    image: 'assets/blue-07.png',
+    imageFront: 'assets/blue-07.png',
+    imageBack: 'assets/back.png',
     name: 'blue 7'
   },
   {
     id: 8,
-    image: 'assets/blue-08.png',
+    imageFront: 'assets/blue-08.png',
+    imageBack: 'assets/back.png',
     name: 'blue 8'
   },
   {
     id: 9,
-    image: 'assets/blue-09.png',
+    imageFront: 'assets/blue-09.png',
+    imageBack: 'assets/back.png',
     name: 'blue 9'
   },
   {
     id: 10,
-    image: 'assets/blue-10.png',
+    imageFront: 'assets/blue-10.png',
+    imageBack: 'assets/back.png',
     name: 'blue 10'
   },
   {
     id: 11,
-    image: 'assets/blue-jack.png',
+    imageFront: 'assets/blue-jack.png',
+    imageBack: 'assets/back.png',
     name: 'blue jack'
   },
   {
     id: 12,
-    image: 'assets/blue-queen.png',
+    imageFront: 'assets/blue-queen.png',
+    imageBack: 'assets/back.png',
     name: 'blue queen'
   },
   {
     id: 13,
-    image: 'assets/blue-king.png',
+    imageFront: 'assets/blue-king.png',
+    imageBack: 'assets/back.png',
     name: 'blue king'
   },
   {
     id: 14,
-    image: 'assets/gold-01.png',
+    imageFront: 'assets/gold-01.png',
+    imageBack: 'assets/back.png',
     name: 'gold A'
   },
   {
     id: 15,
-    image: 'assets/gold-02.png',
+    imageFront: 'assets/gold-02.png',
+    imageBack: 'assets/back.png',
     name: 'gold 2'
   },
   {
     id: 16,
-    image: 'assets/gold-03.png',
+    imageFront: 'assets/gold-03.png',
+    imageBack: 'assets/back.png',
     name: 'gold 3'
   },
   {
     id: 17,
-    image: 'assets/gold-04.png',
+    imageFront: 'assets/gold-04.png',
+    imageBack: 'assets/back.png',
     name: 'gold 3'
   },
   {
     id: 18,
-    image: 'assets/gold-05.png',
+    imageFront: 'assets/gold-05.png',
+    imageBack: 'assets/back.png',
     name: 'gold 5'
   },
   {
     id: 19,
-    image: 'assets/gold-06.png',
+    imageFront: 'assets/gold-06.png',
+    imageBack: 'assets/back.png',
     name: 'gold 6'
   },
   {
     id: 20,
-    image: 'assets/gold-07.png',
+    imageFront: 'assets/gold-07.png',
+    imageBack: 'assets/back.png',
     name: 'gold 7'
   },
   {
     id: 21,
-    image: 'assets/gold-08.png',
+    imageFront: 'assets/gold-08.png',
+    imageBack: 'assets/back.png',
     name: 'gold 8'
   },
   {
     id: 22,
-    image: 'assets/gold-09.png',
+    imageFront: 'assets/gold-09.png',
+    imageBack: 'assets/back.png',
     name: 'gold 9'
   },
   {
     id: 23,
-    image: 'assets/gold-10.png',
+    imageFront: 'assets/gold-10.png',
+    imageBack: 'assets/back.png',
     name: 'gold 10'
   },
   {
     id: 24,
-    image: 'assets/gold-jack.png',
+    imageFront: 'assets/gold-jack.png',
+    imageBack: 'assets/back.png',
     name: 'gold jack'
   },
   {
     id: 25,
-    image: 'assets/gold-queen.png',
+    imageFront: 'assets/gold-queen.png',
+    imageBack: 'assets/back.png',
     name: 'gold queen'
   },
   {
     id: 26,
-    image: 'assets/gold-king.png',
+    imageFront: 'assets/gold-king.png',
+    imageBack: 'assets/back.png',
     name: 'gold king'
   },
   {
     id: 27,
-    image: 'assets/green-01.png',
+    imageFront: 'assets/green-01.png',
+    imageBack: 'assets/back.png',
     name: 'green A'
   },
   {
     id:28,
-    image: 'assets/green-02.png',
+    imageFront: 'assets/green-02.png',
+    imageBack: 'assets/back.png',
     name: 'green 2'
   },
   {
     id: 29,
-    image: 'assets/green-03.png',
+    imageFront: 'assets/green-03.png',
+    imageBack: 'assets/back.png',
     name: 'green 3'
   },
   {
     id: 30,
-    image: 'assets/green-04.png',
+    imageFront: 'assets/green-04.png',
+    imageBack: 'assets/back.png',
     name: 'green 3'
   },
   {
     id: 31,
-    image: 'assets/green-05.png',
+    imageFront: 'assets/green-05.png',
+    imageBack: 'assets/back.png',
     name: 'green 5'
   },
   {
     id: 32,
-    image: 'assets/green-06.png',
+    imageFront: 'assets/green-06.png',
+    imageBack: 'assets/back.png',
     name: 'green 6'
   },
   {
     id: 33,
-    image: 'assets/green-07.png',
+    imageFront: 'assets/green-07.png',
+    imageBack: 'assets/back.png',
     name: 'green 7'
   },
   {
     id: 34,
-    image: 'assets/green-08.png',
+    imageFront: 'assets/green-08.png',
+    imageBack: 'assets/back.png',
     name: 'green 8'
   },
   {
     id: 35,
-    image: 'assets/green-09.png',
+    imageFront: 'assets/green-09.png',
+    imageBack: 'assets/back.png',
     name: 'green 9'
   },
   {
     id: 36,
-    image: 'assets/green-10.png',
+    imageFront: 'assets/green-10.png',
+    imageBack: 'assets/back.png',
     name: 'green 10'
   },
   {
     id: 37,
-    image: 'assets/green-jack.png',
+    imageFront: 'assets/green-jack.png',
+    imageBack: 'assets/back.png',
     name: 'green jack'
   },
   {
     id: 38,
-    image: 'assets/green-queen.png',
+    imageFront: 'assets/green-queen.png',
+    imageBack: 'assets/back.png',
     name: 'green queen'
   },
   {
     id: 39,
-    image: 'assets/green-king.png',
+    imageFront: 'assets/green-king.png',
+    imageBack: 'assets/back.png',
     name: 'green king'
   },
   {
     id: 40,
-    image: 'assets/red-01.png',
+    imageFront: 'assets/red-01.png',
+    imageBack: 'assets/back.png',
     name: 'red A'
   },
   {
     id: 41,
-    image: 'assets/red-02.png',
+    imageFront: 'assets/red-02.png',
+    imageBack: 'assets/back.png',
     name: 'red 2'
   },
   {
     id: 42,
-    image: 'assets/red-03.png',
+    imageFront: 'assets/red-03.png',
+    imageBack: 'assets/back.png',
     name: 'red 3'
   },
   {
     id: 43,
-    image: 'assets/red-04.png',
+    imageFront: 'assets/red-04.png',
+    imageBack: 'assets/back.png',
     name: 'red 3'
   },
   {
     id: 44,
-    image: 'assets/red-05.png',
+    imageFront: 'assets/red-05.png',
+    imageBack: 'assets/back.png',
     name: 'red 5'
   },
   {
     id: 45,
-    image: 'assets/red-06.png',
+    imageFront: 'assets/red-06.png',
+    imageBack: 'assets/back.png',
     name: 'red 6'
   },
   {
     id:46,
-    image: 'assets/red-07.png',
+    imageFront: 'assets/red-07.png',
+    imageBack: 'assets/back.png',
     name: 'red 7'
   },
   {
     id: 47,
-    image: 'assets/red-08.png',
+    imageFront: 'assets/red-08.png',
+    imageBack: 'assets/back.png',
     name: 'red 8'
   },
   {
     id: 48,
-    image: 'assets/red-09.png',
+    imageFront: 'assets/red-09.png',
+    imageBack: 'assets/back.png',
     name: 'red 9'
   },
   {
     id: 49,
-    image: 'assets/red-10.png',
+    imageFront: 'assets/red-10.png',
+    imageBack: 'assets/back.png',
     name: 'red 10'
   },
   {
     id: 50,
-    image: 'assets/red-jack.png',
+    imageFront: 'assets/red-jack.png',
+    imageBack: 'assets/back.png',
     name: 'red jack'
   },
   {
     id: 51,
-    image: 'assets/red-queen.png',
+    imageFront: 'assets/red-queen.png',
+    imageBack: 'assets/back.png',
     name: 'red queen'
   },
   {
     id: 52,
-    image: 'assets/red-king.png',
+    imageFront: 'assets/red-king.png',
+    imageBack: 'assets/back.png',
     name: 'red king'
   }
 ]
