84878925f52bbda9fd2a06005f77ecbb9364fc55
Stephanie Norton <steph.jane.norton@gmail.com>
2021-05-11 11:26:01 -0600

    Delete unused classNames

--- src/components/CardPile/CardPile.js
+++ src/components/CardPile/CardPile.js
@@ -6,23 +6,23 @@ const CardPile = ({ deck }) => {
 
   return (
     <>
-      <div className='card-pile'>
+      <>
         {topCardIndex === deck.length && 
-          <h2 className='user-msg'>You've reached the end of the pile!</h2>
+          <h3 className='user-msg'>You've reached the end of the pile!</h3>
         }
         {topCardIndex !== deck.length &&
           <img className='card-pile-card' src={deck[topCardIndex].imageBack} alt={'top card on the deck'} onClick={() => setTopCardIndex(topCardIndex + 1)} />
         }
-      </div>
+      </>
 
-      <div className='card-active'>
+      <>
         {!topCardIndex &&
-          <h2 className='user-msg'>Click on the card to flip it over!</h2>
+          <h3 className='user-msg'>Click on the card to flip it over!</h3>
         }
         {!!topCardIndex &&
           <img className='card-pile-card' src={deck[topCardIndex - 1].imageFront} alt='flipped card' />
         }
-      </div>
+      </>
     </>
   )
 }
